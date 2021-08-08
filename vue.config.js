f1ac93bbf43bf30cf159649652dbb0a347d84b57
Lawrence856 <sirotinartemtest856@gmail.com>
2021-08-08 17:48:09 +0300

    Правка

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? '/gitogram/dist' : '/'
+    publicPath: '/gitogram/dist'
+    // publicPath: process.env.NODE_ENV === 'production' ? '/gitogram/dist' : '/'
 }
